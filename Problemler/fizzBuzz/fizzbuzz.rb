77a95433dd2d1df26a65d24a8c421286af3cf644
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2016-10-07 14:56:03 +0300

    fizzbuzz

--- Problemler/fizzBuzz/fizzbuzz.rb
+++ Problemler/fizzBuzz/fizzbuzz.rb
@@ -1,9 +1,7 @@
 1.upto(100) do |fizzybuzzy|
      if ( fizzybuzzy%3 == 0 )
              str=""
-             if a%3
-               str= "fizz"
-             end
+             str= "fizz" if a%3
              if a%5
                 str+="buzz"
              else
