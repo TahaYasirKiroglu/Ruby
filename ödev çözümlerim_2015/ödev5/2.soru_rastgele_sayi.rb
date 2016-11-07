5ccff5e0a10897dbd34cab41de63edb713c73d41
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2016-11-07 17:15:45 +0200

    updated

--- ödev çözümlerim_2015/ödev5/2.soru_rastgele_sayi.rb
+++ ödev çözümlerim_2015/ödev5/2.soru_rastgele_sayi.rb
@@ -1,12 +1,19 @@
 #programda yapılan tek değişiklik,
 #programı fonksiyon içine almak oldu
 def rastgele
-  dizi=[]
-  i=0
+  dizi = []
+  i = 0
   while (i<6)
-   dizi[i]=rand(1..49)
-   i += 1
+    dizi[i] = rand(1..49)
+    i += 1
   end
-  print dizi
- end
+end
+print rastgele
+
+def rastgele_v2( i = 6 )
+  dizi=[]
+  i.times { dizi << rand(1..49) }
+  return dizi
+end
+
 print rastgele
