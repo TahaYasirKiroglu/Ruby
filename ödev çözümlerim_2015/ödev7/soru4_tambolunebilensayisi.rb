9e0d9fe9966061766517b2ee909e9861aae62638
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2016-11-08 01:58:46 +0200

    okunabilirlik arttırıldı

--- ödev çözümlerim_2015/ödev7/soru4_tambolunebilensayisi.rb
+++ ödev çözümlerim_2015/ödev7/soru4_tambolunebilensayisi.rb
@@ -1,6 +1,8 @@
-(1..20).each do |bolen|
-  i = 0
-  (1..100).each{ |bolunen| i += 1 if bolunen % bolen==0}
-  puts "1'den 100'e kadar olan sayılardan #{i} tanesi"+
-  " #{bolen} ile tam bölünür."
+(1..20).each do | bolen |
+	i = 0
+	(1..100).each do | bolunen |
+		i += 1 if( bolunen % bolen ) == 0
+		print "1'den 100'e kadar olan sayılardan #{ i } tanesi"
+		puts  " #{ bolen } ile tam bölünür."
+	end
 end
