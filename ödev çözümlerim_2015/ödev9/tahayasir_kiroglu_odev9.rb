753b137991f1b9825be6340f90a8570fbb076ffa
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2016-11-08 03:01:34 +0200

    modülerlk  ve okunabilirlik arttırıldı

--- ödev çözümlerim_2015/ödev9/tahayasir_kiroglu_odev9.rb
+++ ödev çözümlerim_2015/ödev9/tahayasir_kiroglu_odev9.rb
@@ -1,39 +1,48 @@
 #encoding : UTF-8
 require 'stemmify'
-def  oku(dosya)
-  dosya_adi = File.read("#{dosya}.txt").downcase
+def  kucuk_oku( dosya )
+	dosya_adi = File.read( "#{dosya}" ).downcase
 end
-def  sor(a)
-  print a
-  cevap = gets.chomp.to_i
+def  sor( soru )
+	print soru
+	cevap = gets.chomp.to_i
 end
-def  isleme( metin )#alfabe ve bosluk harici karakterleri siler
-  metin = metin.gsub(/([^[a-z]" "é])/, " ").split
+def  alpha_only( metin )#alfabe ve bosluk harici karakterleri siler
+	metin = metin.gsub(/([^[a-z]" "é])/, " ").split
 end
-def  ekle(metin, blank_arr)
-  metin.map! &:to_sym #str to sym
+def  ekle( metin )
+	tekrarlanan = []
+	metin.map! &:to_sym #str to sym
 	metin.each do |karakter|
- 	  blank_arr<< "#{metin.count(:"#{karakter}")} "+" #{karakter}"
-	  metin.delete(karakter)
-  end
+		tekrarlanan<< "#{metin.count(:"#{karakter}")} "+" #{karakter}"
+		metin.delete(karakter)
+	end
+	return tekrarlanan
 end
-def  gereksiz_sil(silinecek, metin)
-	silinecek.each { |sil| metin.delete("#{sil}")}
-	metin.map!{ |kelime| kelime.stem }#kelimeleri köklerine kadar ayrıstırır
-	metin = metin.each { |kelime| metin.delete("#{kelime}") if  kelime.size < 3}
+def  cop_cikar( copler, metin )
+	copler.each { | cop | metin.delete("#{ cop }")} # copleri metinden cikarir
+	return metin
+end
+def  kokunu_kazi( metin )
+	metin.map!{ |kelime| kelime.stem } #kelimeleri köklerine kadar ayrıstırır
+	metin = metin.each { |kelime| metin.delete("#{ kelime }") if  kelime.size < 3}
 end
 def  donustur(arr)#mevcut stringleri istenilen ölçüye dönüştür
-  arr.map! do |at|
-    at = at.split
-	  at = "#{at[1]}\n#{at[0]}"
-  end
+	arr.map! do |at|
+		at = at.split
+		at = "#{at[1]}\n#{at[0]}"
+	end
 end
-def  calıstır(sayi, arr)#verilen sayida argümanı ekrana bas
-  puts arr.take(sayi)
+def  puts_arr(sayi, arr)#verilen sayida argümanı ekrana bas
+	puts arr.take( sayi )
 end
-metin, wipe = isleme(oku("metin")), oku("stp").split #metin.txt ve stp.txt'i oku ve işle
-gereksiz_sil(wipe, metin) #vasıfsız karakterleri sil
-wipe=[]
-ekle(metin, wipe)
-wipe.sort!.reverse! #yüksek sayılı başta olacak şekilde sırala
-calıstır(sor("Eşik değeri giriniz: "), donustur(wipe))
+def  asil_musteri?( metin, cop ) # en sık gecen kelimeleri bulur
+	metin = alpha_only( kucuk_oku( metin ) ) 
+	copler = kucuk_oku( cop ).split #metin.txt ve stp.txt'i oku ve işle
+	metin = cop_cikar( copler, metin) #vasıfsız karakterleri sil
+	kokunu_kazi( metin ) 
+	metin = ekle( metin )
+	metin.sort!.reverse! #yüksek sayılı başta olacak şekilde sırala
+	puts_arr( sor("Eşik değeri giriniz: "), donustur(metin) )
+end
+asil_musteri?( "metin.txt", "stp.txt" )
