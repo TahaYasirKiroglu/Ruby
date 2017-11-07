8a8790146504d94f3ce2f747c875aa850a66b798
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2017-11-07 19:59:54 +0300

    Update fizzbuzz.rb

--- Problemler/fizzBuzz/fizzbuzz.rb
+++ Problemler/fizzBuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
 1.upto(100) do |fizzybuzzy|
-     if ( fizzybuzzy%3 == 0 )
-             str=""
-             str= "fizz" if a%3
-             if a%5
-                str+="buzz"
-             else
-                str = "#{fizzbuzz}"
-             end
-             puts str
+  if ( fizzybuzzy%3 == 0 )
+    str=""
+    str= "fizz" if a%3
+    if a%5
+      str+="buzz"
+    else
+      str = "#{fizzbuzz}"
+    end
+    puts str
 end
